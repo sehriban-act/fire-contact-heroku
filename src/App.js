6682f5c0ac2626efbc81c4e27f9b4c8ea082c51e
sehriban <sehribanmatur9@gmail.com>
2021-12-26 16:40:43 -0500

    starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+// import FormComponent from './components/form/FormComponent';
+// import Contacts from './components/contacts/Contacts';
+
+function App() {
+  return (
+    <div className="App">
+  
+    </div>
+  );
+}
+
+export default App;
